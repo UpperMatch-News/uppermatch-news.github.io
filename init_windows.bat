d4aa804977eba6e55297255c367d2a067654cf2d
TobyVanBuynder <tobyvbuynder@gmail.com>
2025-05-30 11:38:29 +0200

    Remove init scripts

--- init_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Set the execution policy to allow running scripts if it's not already allowed
-PowerShell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force"
-
-:: Run the PowerShell script
-PowerShell -ExecutionPolicy Bypass -File "%~dp0__init\windows.ps1"
-
-pause
